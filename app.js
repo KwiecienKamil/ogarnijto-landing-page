4d9e5f801310d26a1c767c7d72785f69669ffbfe
Kamil <kamilkwiecien9@gmail.com>
2025-08-23 15:08:02 +0200

    Added responsive nav functionality

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
  const menu = document.getElementById("main-nav");
   const menuBtn = document.querySelector(".fa-bars");
-
+const closeBtn = document.querySelector(".close-nav");
   menuBtn.addEventListener("click", () => {
     menu.classList.toggle("active");
     menuBtn.classList.toggle("fa-xmark");
   });
+
+  closeBtn.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
