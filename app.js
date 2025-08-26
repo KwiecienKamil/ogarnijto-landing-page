6dd9832d2669bf9bbb0452acaa090eb770745eaa
Kamil <kamilkwiecien9@gmail.com>
2025-08-26 20:11:04 +0200

    Style review components + animations

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ const closeBtn = document.querySelector(".close-nav");
   closeBtn.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+
+
+window.addEventListener("scroll", () => {
+    const blob = document.getElementById("background-decoration-blob");
+    if (window.scrollY > 50) {
+      blob.style.backgroundColor = "#111827"; 
+    } else {
+      blob.style.backgroundColor = "#e48d8d"; 
+    }
+  });
